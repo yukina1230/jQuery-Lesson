ccda7efe8fc50ab58a7f0991219098ba5100d494
yukina1230 <lost.laugh1230@gmail.com>
2021-03-21 10:18:34 +0900

    [Update]slidedown&slideup

--- app.js
+++ app.js
@@ -1,4 +1,9 @@
-$(function(){
-  $('.box1').show();
-  $('.box1').css({'background-color': '#0000ff'});
+$(function (){
+  $('.box1').slideDown(function (){
+    $('.box1').css({
+      'background-color': '#0000FF',
+      'width': '200px',
+      'height': '100px'
+    }).slideUp();
+  });
 });
