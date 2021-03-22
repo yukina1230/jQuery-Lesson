1bf0c7ef362c65a3f83b0d81d8bf6576e091973c
yukina1230 <lost.laugh1230@gmail.com>
2021-03-22 09:07:20 +0900

    [Update]add_remove_class

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 $(function(){
   $('.box1').mouseover(function(){
-    $('.box1').css({'background-color': '#0000ff'});
+    $('.box1').addClass('box1-ext');
   });
   $('.box1').mouseout(function(){
-    $('.box1').css({'background-color': '#ff0000'});
+    $('.box1').removeClass('box1-ext');
   });
 });
