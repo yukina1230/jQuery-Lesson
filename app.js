033c40850d0586dbb29b32f969e8ae7bbcc855e4
yukina1230 <lost.laugh1230@gmail.com>
2021-03-22 09:03:51 +0900

    [Update]mouseover_mouseout

--- app.js
+++ app.js
@@ -1,9 +1,8 @@
-$(function (){
-  $('.box1').slideDown(function (){
-    $('.box1').css({
-      'background-color': '#0000FF',
-      'width': '200px',
-      'height': '100px'
-    }).slideUp();
+$(function(){
+  $('.box1').mouseover(function(){
+    $('.box1').css({'background-color': '#0000ff'});
+  });
+  $('.box1').mouseout(function(){
+    $('.box1').css({'background-color': '#ff0000'});
   });
 });
